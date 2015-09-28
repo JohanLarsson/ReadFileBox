a530e5ac2dde55571d99dd728bf999799a7f4fbc
Johan Larsson <johan_gavle1@hotmail.com>
2015-09-28 08:42:18 +0200

    Initial

---
+++ ReadFileBox/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace ReadFileBox
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ViewModel _vm = new ViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _vm;
+        }
+
+        private async void OnReadClick(object sender, RoutedEventArgs e)
+        {
+            await _vm.Read().ConfigureAwait(true);
+        }
+
+        private async void OnSaveClick(object sender, RoutedEventArgs e)
+        {
+            await _vm.Save().ConfigureAwait(true);
+        }
+    }
+}
---
+++ ReadFileBox/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿namespace ReadFileBox
+{
+    using System.ComponentModel;
+    using System.IO;
+    using System.Runtime.CompilerServices;
+    using System.Threading.Tasks;
+    using ReadFileBox.Annotations;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string _content;
+        private static readonly string FileName = @"C:\Temp\SampleFile.txt";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Content
+        {
+            get { return _content; }
+            set
+            {
+                if (value == _content) return;
+                _content = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async Task Read()
+        {
+            using (var stream = File.OpenRead(FileName))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    Content = await reader.ReadToEndAsync().ConfigureAwait(false);
+                }
+            }
+        }
+
+        public async Task Save()
+        {
+            using (var stream = File.OpenWrite(FileName))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    await writer.WriteAsync(Content).ConfigureAwait(false);
+                }
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
